665c68c9bff585d4ad814f3129b446da096e34df
lilf4 <danielfandersen@hotmail.com>
2023-10-10 09:03:45 +0200

    DM1 - Created product class

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+namespace ENF_Dist_Test
+{
+    public class Product{
+        int ProductId {get; set;}
+        string Name {get; set;} = string.Empty;
+        int Count {get; set;}
+        string Description {get; set;} = string.Empty;
+        string LocationId {get; set;} = string.Empty;
+    }
+}
