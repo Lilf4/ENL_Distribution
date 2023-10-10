6796b7b089a518698550bfc9f57bfe74fe44d4c7
lilf4 <danielfandersen@hotmail.com>
2023-10-10 09:15:27 +0200

    DM3 - added defaults to string

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -5,10 +5,10 @@ namespace ENF_Dist_Test
     public class Employee{
         int EmployeeId {get; set;}
         int CompletedOrders {get; set;}
-        string FirstName {get; set;}
-        string LastName {get; set;}
-        string PhoneNumber {get; set;}
-        string Email {get; set;}
+        string FirstName {get; set;} = string.Empty;
+        string LastName {get; set;} = string.Empty;
+        string PhoneNumber {get; set;} = string.Empty;
+        string Email {get; set;} = string.Empty;
         JobTitles JobTitle {get; set;} 
 
         enum JobTitles
