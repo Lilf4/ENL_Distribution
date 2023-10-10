8977b0acfcfb2fc8eda743a8970a810483f49e9f
lilf4 <danielfandersen@hotmail.com>
2023-10-10 14:02:38 +0200

    D2, D3, D4 - Added crud functions

--- Database.cs
+++ Database.cs
@@ -1,8 +1,62 @@
+using System.Collections.Generic;
+
 namespace ENF_Dist_Test
 {
     public class Database{
-        public static Database Instance = new Database();
+        public static readonly Database Instance = new Database();
 
-        
+        #region Product
+        public Product GetProduct(int ProductId){
+            return null;
+        }
+        public List<Product> GetAllProducts(){
+            return null;
+        }
+        public int InsertProduct(Product Product){
+            return -1;
+        }
+        public int UpdateProduct(Product Product, int ProductId){
+            return -1;
+        }
+        public int DeleteProduct(int ProductId){
+            return -1;
+        }
+        #endregion
+
+        #region Employee
+        public Product GetEmployee(int EmployeeId){
+            return null;
+        }
+        public List<Product> GetAllEmployees(){
+            return null;
+        }
+        public int InsertEmployee(Employee Employee){
+            return -1;
+        }
+        public int UpdateEmployee(Employee Employee, int EmployeeId){
+            return -1;
+        }
+        public int DeleteEmployee(int EmployeeId){
+            return -1;
+        }
+        #endregion
+
+        #region Order
+        public Product GetOrder(int OrderId){
+            return null;
+        }
+        public List<Product> GetAllOrders(){
+            return null;
+        }
+        public int InsertOrder(Order Order){
+            return -1;
+        }
+        public int UpdateOrder(Order Order, int OrderId){
+            return -1;
+        }
+        public int DeleteOrder(int OrderId){
+            return -1;
+        }
+        #endregion
     }
 }
