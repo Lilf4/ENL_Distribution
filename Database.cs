2ad61fb2411f76cc29d3242f17dc59fc71e8faec
lilf4 <danielfandersen@hotmail.com>
2023-10-10 09:38:02 +0200

    D1 - created database class and made a static instance

--- Database.cs
+++ Database.cs
@@ -1,10 +1,8 @@
 namespace ENF_Dist_Test
 {
     public class Database{
-        public static Database Instance;
+        public static Database Instance = new Database();
 
-        public Database(){
-            Instance = new Database();
-        }
+        
     }
 }
