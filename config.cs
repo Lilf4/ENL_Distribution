a5e5f09e487a8189487e36dde0bfd4f4e9151e32
lilf4 <danielfandersen@hotmail.com>
2023-10-13 12:49:52 +0200

    U2 - Created config class

---
+++ config.cs
@@ -0,0 +1,8 @@
+﻿namespace ENF_Dist_Test {
+    internal class config {
+        public string dataSource { get; set; }
+        public string dataBase { get; set; }
+        public string UserID { get; set; }
+        public string password { get; set; }
+    }
+}
