5a2792133eb354bf0a9c59f4daa088e817e3635c
lilf4 <danielfandersen@hotmail.com>
2023-10-16 14:57:16 +0200

    U2 - Added validation classes

--- Validators/OrderValidator.cs
+++ Validators/OrderValidator.cs
@@ -3,7 +3,9 @@ using FluentValidation;
 namespace ENF_Dist_Test.Validators {
     public class OrderValidator : AbstractValidator<Order> {
         public OrderValidator() {
-            RuleFor(order =>  order.Quantity).GreaterThanOrEqualTo(0);
+            RuleFor(order => order.Quantity).GreaterThanOrEqualTo(0);
+            RuleFor(order => order.Employee).NotNull();
+            RuleFor(order => order.Product).NotNull();
         }
     }
 }
---
+++ Validators/ProductValidator.cs
@@ -0,0 +1,13 @@
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace ENF_Dist_Test.Validators {
+    public class ProductValidator : AbstractValidator<Product> {
+        public ProductValidator() {
+            RuleFor(product => product.Name).NotEmpty();
+            RuleFor(product => product.Description).NotEmpty();
+            RuleFor(product => product.Quantity).GreaterThanOrEqualTo(0);
+
+        }
+    }
+}
