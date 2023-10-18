bc907cfa1e9783539ece279df2323fa0f56d7875
lilf4 <danielfandersen@hotmail.com>
2023-10-18 14:39:23 +0200

    U2 - Added button to change faker settings

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using ENF_Dist_Test.Validators;
+using ENF_Dist_Test.Windows;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -18,6 +20,17 @@ namespace ENF_Dist_Test.Pages{
         private void NavOrder(object sender, RoutedEventArgs e) {
             NavigationService.Navigate(new OrderPage());
         }
+        private void RecreateDB(object sender, RoutedEventArgs e) {
+            FakerInterface FI = new FakerInterface();
+            if (FI.ShowDialog() == true) {
+                if (new confirm("Recreate database?", "Are you sure you want to recreate the database?\r\nThis can take a while.").ShowDialog() == true) {
+                    FakerClass fakerClass = new FakerClass();
+                    Database.Instance.RebuildDatabase();
+                    fakerClass.generateData(FI.GenerateInfo);
+                }
+            }
+        }
+
         private void NavExit(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
