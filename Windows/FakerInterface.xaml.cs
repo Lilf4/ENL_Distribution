4c9e896b526ec2ab472b0a4f814e62357b6d2131
lilf4 <danielfandersen@hotmail.com>
2023-10-18 14:39:01 +0200

    U2 - Added Window for interfacing with faker settings

---
+++ Windows/FakerInterface.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ENF_Dist_Test.Validators;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ENF_Dist_Test.Windows {
+    public partial class FakerInterface : Window, INotifyPropertyChanged {
+        private fakerGenerateInfo generateInfo { get; set; }
+        public fakerGenerateInfo GenerateInfo {
+            get {
+                return generateInfo;
+            }
+            set {
+                generateInfo = value;
+                OnPropertyChanged(nameof(GenerateInfo));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public FakerInterface() {
+            InitializeComponent();
+            GenerateInfo = new();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.ChangedButton == MouseButton.Left) {
+                this.DragMove();
+            }
+        }
+
+        private void FinishBtn_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
